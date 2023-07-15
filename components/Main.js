3572112ffdb99bda825f42fdd99c67343ac20b86
satyam <satyamremote@gmail.com>
2023-07-15 21:19:57 +0530

    Initial commit

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">About Me</h1>
+            <ul className="main--facts">
+                <li>My name is Satyam Gupta</li>
+                <li>I am a Front-End Web Developer</li>
+                <li>I am a Computer Science undergraduate</li>
+                <li>I am currently 19 years old and I play Football</li>
+            </ul>
+        </main>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/react-icon-small.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactStatic</h3>
+            <h4 className="nav--title">PORTFOLIO WEB</h4>
+        </nav>
+    )
+}
